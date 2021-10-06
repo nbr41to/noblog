b4dd2e564f133cb24fae61e1f18a8ea45432a750
nbr41to <nbr.41to@gmail.com>
2021-10-06 20:53:41 +0900

    ogp取得の修正

--- src/hooks/useOgp.ts
+++ src/hooks/useOgp.ts
@@ -6,9 +6,8 @@ const fetcher = (url: string): Promise<any> =>
   fetch(url).then((res) => res.json());
 
 export const useOgp = (url: string): any => {
-  const key = `/api/get/ogp/${url
-    .replace('https://', '')
-    .replace('http://', '')}`;
+  const encoded = encodeURIComponent(url);
+  const key = `/api/get/ogp/${encoded}`;
   const { data, error } = useSWR(key, fetcher);
   if (error) console.error(error);
   return { data };
