7940b38e86e97a411000d5c7111f5db4a08473b7
nbr41to <nbr.41to@gmail.com>
2021-10-11 16:45:31 +0900

    ISRに変更してImageを検証

--- pages/blogs/[pageId].tsx
+++ pages/blogs/[pageId].tsx
@@ -1,9 +1,6 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import Head from 'next/head';
-import { useMemo } from 'react';
 import { getPageContent } from 'src/apis/notion';
 import { getMainBlogList } from 'src/apis/notion';
-import { dateFormatted } from 'src/utils/dateFormatted';
 import styled from 'styled-components';
 
 import { BlogDetail } from '@/components/Blog/BlogDetail';
@@ -15,12 +12,26 @@ type BlogDetailPageProps = {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { pageId } = params;
-  const content = await getPageContent(
-    typeof pageId === 'string' ? pageId : pageId[0],
-  );
-  return {
-    props: { content },
-  };
+  try {
+    const content = await getPageContent(
+      typeof pageId === 'string' ? pageId : pageId[0],
+    );
+    /* contentがなくてもErrorでも404へ */
+    if (!content) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: { content },
+      revalidate: 3600, // 60s * 60min
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      notFound: true,
+    };
+  }
 };
 
 type Params = {
@@ -32,58 +43,15 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
   const paths = pageList.map(({ id }) => ({ params: { pageId: id } }));
   return {
     paths,
-    fallback: false, // 事前に予測されたpathでなければ404を返す
-    /* https://maku.blog/p/rdq3ep2/ */
+    fallback: 'blocking', // HTMLを生成しない
   };
 };
 
 const BlogDetailPage: NextPage<BlogDetailPageProps> = ({ content }) => {
-  /* contentを抽出してmemo化 */
-  const extractedContent = useMemo(() => {
-    const { pageInfo } = content;
-    const { properties } = pageInfo;
-    const { blocks } = content;
-
-    const extractedContent = {
-      id: pageInfo.id,
-      title: '',
-      icon: '👏',
-      cover_url: '',
-      created_time: dateFormatted({ date: pageInfo.created_time }),
-      last_edited_time: dateFormatted({ date: pageInfo.last_edited_time }),
-      category: null,
-      tags: [],
-      blocks,
-    };
-    /* いでよ！型ガードたちよ */
-    if (pageInfo.icon && pageInfo.icon.type === 'emoji') {
-      extractedContent.icon = pageInfo.icon.emoji;
-    }
-    if (pageInfo.cover && pageInfo.cover.type === 'external') {
-      extractedContent.cover_url = pageInfo.cover.external.url;
-    }
-    if (properties.Title.type === 'title') {
-      extractedContent.title = properties.Title.title[0].plain_text;
-    }
-    if (properties.Category.type === 'select') {
-      extractedContent.category = properties.Category.select;
-    }
-    if (properties.Tags.type === 'multi_select') {
-      extractedContent.tags = properties.Tags.multi_select;
-    }
-
-    return extractedContent;
-  }, []);
-
   return (
-    <>
-      <Head>
-        <title>{`noblog | ${extractedContent.title}`}</title>
-      </Head>
-      <BlogDetailPageStyled>
-        <BlogDetail detail={extractedContent} />
-      </BlogDetailPageStyled>
-    </>
+    <BlogDetailPageStyled>
+      <BlogDetail content={content} />
+    </BlogDetailPageStyled>
   );
 };
 
--- src/components/Blog/BlogDetail.tsx
+++ src/components/Blog/BlogDetail.tsx
@@ -1,49 +1,75 @@
-import { VFC } from 'react';
+import Head from 'next/head';
+import { useMemo, VFC } from 'react';
 import styled from 'styled-components';
 
 import { AdsenseRow } from '@/components/Adsense/AdsenseRow';
 import { NotionBlockCompiler } from '@/components/Notion/NotionBlockCompiler';
-import { NotionBlock, NotionColor } from '@/type/notion';
+import { NotionPageContent } from '@/type/notion';
+import { dateFormatted } from '@/utils/dateFormatted';
 
 import { DetailHeader } from './_DetailHeader';
 
 type BlogDetailProps = {
   className?: string;
-  detail: {
-    id: string;
-    title: string;
-    icon: string;
-    cover_url: string;
-    created_time: string;
-    last_edited_time: string;
-    category: {
-      id: string;
-      color: NotionColor;
-      name: string;
-    };
-    tags: {
-      id: string;
-      color: NotionColor;
-      name: string;
-    }[];
-    blocks: NotionBlock[];
-  };
+  content: NotionPageContent;
 };
 
-export const BlogDetail: VFC<BlogDetailProps> = ({ className, detail }) => {
-  const { blocks, ...headerInfo } = detail;
+export const BlogDetail: VFC<BlogDetailProps> = ({ className, content }) => {
+  /* contentを抽出してmemo化 */
+  const extractedContent = useMemo(() => {
+    const { pageInfo } = content;
+    const { properties } = pageInfo;
+    const { blocks } = content;
+
+    const extractedContent = {
+      id: pageInfo.id,
+      title: '',
+      icon: '👏',
+      cover_url: '',
+      created_time: dateFormatted({ date: pageInfo.created_time }),
+      last_edited_time: dateFormatted({ date: pageInfo.last_edited_time }),
+      category: null,
+      tags: [],
+      blocks,
+    };
+    /* いでよ！型ガードたちよ */
+    if (pageInfo.icon && pageInfo.icon.type === 'emoji') {
+      extractedContent.icon = pageInfo.icon.emoji;
+    }
+    if (pageInfo.cover && pageInfo.cover.type === 'external') {
+      extractedContent.cover_url = pageInfo.cover.external.url;
+    }
+    if (properties.Title.type === 'title') {
+      extractedContent.title = properties.Title.title[0].plain_text;
+    }
+    if (properties.Category.type === 'select') {
+      extractedContent.category = properties.Category.select;
+    }
+    if (properties.Tags.type === 'multi_select') {
+      extractedContent.tags = properties.Tags.multi_select;
+    }
+
+    return extractedContent;
+  }, []);
+
+  const { blocks, ...headerInfo } = extractedContent;
 
   return (
-    <StyledBlogDetail className={`${className}`}>
-      <DetailHeader {...headerInfo} />
-      <div className="detail_body">
-        {blocks.map((block) => (
-          <NotionBlockCompiler key={block.id} block={block} />
-        ))}
-        {/* <AdsenseRow /> */}
-      </div>
-      <div className="detail_footer"></div>
-    </StyledBlogDetail>
+    <>
+      <Head>
+        <title>{`noblog | ${extractedContent.title}`}</title>
+      </Head>
+      <StyledBlogDetail className={`${className}`}>
+        <DetailHeader {...headerInfo} />
+        <div className="detail_body">
+          {blocks.map((block) => (
+            <NotionBlockCompiler key={block.id} block={block} />
+          ))}
+          {/* <AdsenseRow /> */}
+        </div>
+        <div className="detail_footer"></div>
+      </StyledBlogDetail>
+    </>
   );
 };
 
