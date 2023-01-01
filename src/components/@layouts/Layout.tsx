d8c5be7064d9e25f62a324ca4e987ce8805dfe0d
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-01 20:52:13 +0900

    footerをいい感じに

--- src/components/@layouts/Layout.tsx
+++ src/components/@layouts/Layout.tsx
@@ -1,5 +1,7 @@
 import type { FC, ReactNode } from 'react';
 
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { NavMenu } from './NavMenu';
@@ -14,7 +16,7 @@ export const Layout: FC<Props> = ({ children }) => {
 
   return (
     <div>
-      <div className="fixed z-10">
+      <div className="fixed z-50">
         <NavMenu />
       </div>
 
@@ -27,12 +29,31 @@ export const Layout: FC<Props> = ({ children }) => {
         </div>
       </header>
 
-      <main className="mx-auto min-h-[calc(100vh-102px)] max-w-[1200px] pb-16">
+      <main className="relative z-10 mx-auto mb-40 min-h-[calc(100vh-102px)] max-w-[1200px] bg-orange-100 pb-20">
         {children}
       </main>
 
-      <footer className="w-full bg-slate-800 py-1 text-center text-white">
-        <small>@2023</small>
+      <footer className="fixed bottom-0 flex h-40 w-full flex-col justify-between bg-slate-800 px-8 text-white">
+        <div className="mt-6">
+          <div className="flex gap-2">
+            <Image
+              src="/logo_circle.png"
+              width={32}
+              height={32}
+              alt="site logo"
+            />
+            <Link href="/" className="font-baloo text-3xl text-white">
+              noblog
+            </Link>
+          </div>
+          <div className="mt-2 text-xs">
+            Notion API と Next.js Tailwind CSS で本格ブログを作ってみました。
+          </div>
+        </div>
+
+        <div className="py-2 text-center text-xs font-bold text-slate-200">
+          Made with Notion by nobuyuki @2023
+        </div>
       </footer>
 
       <ScrollTopButton />
