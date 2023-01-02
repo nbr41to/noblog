8b0659439c70bacb5c75000d46f9f89f508869e8
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-02 16:55:35 +0900

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,5 +8,6 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 export default NextAuth(authOptions);
