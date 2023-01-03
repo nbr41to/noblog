4b72cfcb5c7d6bcbd6ff875d735edc6ae5262905
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-04 04:59:48 +0900

    検索機能の向上

--- src/utils/algolia.ts
+++ src/utils/algolia.ts
@@ -1,3 +1,5 @@
+import type { AlgoliaSearchObjectResponse } from '~/types/algolia';
+
 import algoliasearch from 'algoliasearch/lite';
 
 const client = algoliasearch(
@@ -10,7 +12,8 @@ const index = client.initIndex('noblog');
 export const searchAlgolia = async (query: string) => {
   const res = await index.search(query, {
     attributesToRetrieve: ['title', 'category', 'tags'],
+    // hitsPerPage: 20,
   });
 
-  return res;
+  return res.hits as AlgoliaSearchObjectResponse[];
 };
