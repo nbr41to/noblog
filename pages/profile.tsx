11fcabba04a0d2c634a233c3aef38de6fce8c158
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-08 13:23:57 +0900

    dummyを追加

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,17 +1,18 @@
 import type { InferGetStaticPropsType, NextPage } from 'next';
 import type { NotionBlockObjectResponse } from '~/types/notion';
 
+import notion_profile_blocks from '~/mocks/notion_profile_blocks.json';
 import { getChildrenInBlock } from '~/server/notion/blocks';
 import { ProfileTemplate } from '~/templates/ProfileTemplate';
 
 export const getStaticProps = async () => {
-  // if (process.env.ENVIRONMENT === 'local') {
-  //   return {
-  //     props: {
-  //       posts: dummy_notion_pages_latest,
-  //     },
-  //   };
-  // }
+  if (process.env.ENVIRONMENT === 'local') {
+    return {
+      props: {
+        blocks: notion_profile_blocks as NotionBlockObjectResponse[],
+      },
+    };
+  }
 
   const { results } = await getChildrenInBlock({
     block_id: process.env.NOTION_PROFILE_PAGE_ID || '',
@@ -21,7 +22,6 @@ export const getStaticProps = async () => {
     props: {
       blocks: results as NotionBlockObjectResponse[],
     },
-    revalidate: 60 * 60 * 24, // 1日
   };
 };
 
