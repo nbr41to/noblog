e68c2bd20bcb7a587825bff74d8109e2ee94dc6c
Knob <59198478+nbr41to@users.noreply.github.com>
2023-02-07 21:43:40 +0900

    Create input-file-list.tsx

---
+++ pages/sandbox/input-file-list.tsx
@@ -0,0 +1,179 @@
+/* eslint-disable no-console */
+import type { NextPage } from 'next';
+
+import { Prism } from '@mantine/prism';
+import Head from 'next/head';
+import { useMemo, useRef, useState } from 'react';
+
+import { PageTitle } from '~/commons/PageTitle';
+
+const MyInputFileListComponent = () => {
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputFileListComponent inputFiles', inputFiles);
+
+  return <input type="file" onChange={(e) => setInputFiles(e.target.files)} />;
+};
+const MyInputFileComponent = () => {
+  const [inputFiles, setInputFiles] = useState<File[]>([]);
+  console.log('MyInputFileComponent inputFiles', inputFiles);
+
+  return (
+    <input
+      type="file"
+      onChange={(e) =>
+        setInputFiles(e.target.files ? [...Array.from(e.target.files)] : [])
+      }
+    />
+  );
+};
+const MyInputMultiFileListComponent = () => {
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputMultiFileListComponent inputFiles', inputFiles);
+
+  return (
+    <input
+      type="file"
+      multiple
+      onChange={(e) => setInputFiles(e.target.files)}
+    />
+  );
+};
+const MyInputMultiFileListControlComponent = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputMultiFileListControlComponent inputFiles', inputFiles);
+
+  const selectedFileArray: File[] = useMemo(() => {
+    return inputFiles ? [...Array.from(inputFiles)] : [];
+  }, [inputFiles]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    if (!inputRef.current?.files) return;
+    const newFileArray = [
+      ...selectedFileArray,
+      ...Array.from(e.target.files),
+    ].filter(
+      (file, index, self) =>
+        self.findIndex((f) => f.name === file.name) === index // 重複を削除
+    );
+    const dt = new DataTransfer();
+    newFileArray.forEach((file) => dt.items.add(file));
+    inputRef.current.files = dt.files; // input内のFileListを更新
+    setInputFiles(dt.files); // Reactのstateを更新
+  };
+
+  const handleDelete = (index: number) => {
+    if (!inputRef.current?.files) return;
+    const dt = new DataTransfer();
+    selectedFileArray.forEach((file, i) => i !== index && dt.items.add(file));
+    inputRef.current.files = dt.files; // input内のFileListを更新
+    setInputFiles(dt.files); // Reactのstateを更新
+  };
+
+  return (
+    <div>
+      <input type="file" multiple onChange={handleChange} ref={inputRef} />
+      <div className="w-fit space-y-3">
+        {selectedFileArray.map((file, index) => (
+          <div
+            key={file.name}
+            className="flex items-center justify-between gap-2"
+          >
+            <div>{file.name}</div>
+            <button onClick={() => handleDelete(index)}>削除</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const InputFileList: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <PageTitle title="input multiple FileList" />
+
+      <div className="w-main mx-auto mt-8 space-y-3">
+        <h3>MyInputFileListComponent</h3>
+        <MyInputFileListComponent />
+        <Prism language="tsx">
+          {`const MyInputFileListComponent = () => {
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputFileListComponent inputFiles', inputFiles);
+
+  return <input type="file" onChange={(e) => setInputFiles(e.target.files)} />;
+};`}
+        </Prism>
+        <h3>MyInputFileComponent</h3>
+        <Prism language="tsx">{`const MyInputFileComponent = () => {
+  const [inputFiles, setInputFiles] = useState<File[]>([]);
+  console.log('MyInputFileComponent inputFiles', inputFiles);
+
+  return (
+    <input
+      type="file"
+      onChange={(e) =>
+        setInputFiles(e.target.files ? [...Array.from(e.target.files)] : [])
+      }
+    />
+  );
+};`}</Prism>
+        <MyInputFileComponent />
+        <h3>MyInputMultiFileListComponent</h3>
+        <MyInputMultiFileListComponent />
+        <Prism language="tsx">{`const MyInputMultiFileListComponent = () => {
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputMultiFileListComponent inputFiles', inputFiles);
+
+  return (
+    <input
+      type="file"
+      multiple
+      onChange={(e) => setInputFiles(e.target.files)}
+    />
+  );
+};`}</Prism>
+        <h3>MyInputMultiFileListControlComponent</h3>
+        <MyInputMultiFileListControlComponent />
+        <Prism language="tsx">{`const MyInputMultiFileListControlComponent = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputFiles, setInputFiles] = useState<FileList | null>(null);
+  console.log('MyInputMultiFileListControlComponent inputFiles', inputFiles);
+
+  const selectedFileArray: File[] = useMemo(() => {
+    return inputFiles ? [...Array.from(inputFiles)] : [];
+  }, [inputFiles]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    if (!inputRef.current?.files) return;
+    const newFileArray = [
+      ...selectedFileArray,
+      ...Array.from(e.target.files),
+    ].filter(
+      (file, index, self) =>
+        self.findIndex((f) => f.name === file.name) === index // 重複を削除
+    );
+    const dt = new DataTransfer();
+    newFileArray.forEach((file) => dt.items.add(file));
+    inputRef.current.files = dt.files; // input内のFileListを更新
+    setInputFiles(dt.files); // Reactのstateを更新
+  };
+
+  const handleDelete = (index: number) => {
+    if (!inputRef.current?.files) return;
+    const dt = new DataTransfer();
+    selectedFileArray.forEach((file, i) => i !== index && dt.items.add(file));
+    inputRef.current.files = dt.files; // input内のFileListを更新
+    setInputFiles(dt.files); // Reactのstateを更新
+  };`}</Prism>
+      </div>
+    </div>
+  );
+};
+
+export default InputFileList;
