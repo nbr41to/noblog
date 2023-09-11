0aa8d003916629f6dcc92e653cd3556833476c19
Knob <59198478+nbr41to@users.noreply.github.com>
2023-09-11 18:38:08 +0900

    Update og.tsx

--- pages/api/notion-blog/og.tsx
+++ pages/api/notion-blog/og.tsx
@@ -7,32 +7,68 @@ export const config = {
 };
 
 export default async function handler(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
+  try {
+    const { searchParams } = new URL(req.url);
 
-  const hasTitle = searchParams.has('title');
-  const title = hasTitle ? searchParams.get('title')?.slice(0, 50) : '';
+    const hasTitle = searchParams.has('title');
+    const title = hasTitle ? searchParams.get('title')?.slice(0, 50) : '';
 
-  return new ImageResponse(
-    (
-      <div tw="relative flex h-full w-full flex-col flex-wrap items-center justify-center bg-orange-100 text-slate-800 font-baloo">
-        {title ? (
-          <div tw="w-[1000px] whitespace-pre-wrap text-[72px] font-bold mx-auto">
-            {title}
-          </div>
-        ) : (
-          <div tw="text-[140px]">noblog</div>
-        )}
-        {title ? (
-          <div tw="absolute bottom-2 right-6 text-[72px]">noblog</div>
-        ) : (
-          <div tw="h-2 w-60 rounded-full bg-slate-800" />
-        )}
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-      emoji: 'fluentFlat',
-    },
-  );
+    return new ImageResponse(
+      (
+        <div tw="relative flex h-full w-full flex-col flex-wrap items-center justify-center bg-orange-100 text-slate-800">
+          {title ? (
+            <div tw="w-[1000px] whitespace-pre-wrap text-[72px] font-bold mx-auto">
+              {title}
+            </div>
+          ) : (
+            <div tw="text-[140px]">のぶろぐ</div>
+          )}
+          {title ? (
+            <div tw="absolute bottom-2 right-6 text-[72px]">のぶろぐ</div>
+          ) : (
+            <div tw="h-2 w-60 rounded-full bg-slate-800" />
+          )}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        emoji: 'fluentFlat',
+      },
+    );
+  } catch (error) {
+    const imageData = await fetch(
+      new URL('../../../public/noblog_og.png', import.meta.url),
+    );
+    const buffer = await imageData.arrayBuffer();
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            display: 'flex',
+            background: '#f6f6f6',
+            width: '100%',
+            height: '100%',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            // @ts-expect-error @vercel/ogの仕様都合
+            src={buffer}
+            width="256"
+            height="256"
+            alt="og image"
+          />{' '}
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  }
 }
